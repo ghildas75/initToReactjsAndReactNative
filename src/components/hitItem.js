b76648688d36d2f7dd5c0e06ef6a240e693c1e4e
Samir GHILAS <samirg@evision.ca>
2020-05-03 23:26:32 -0400

    update back

--- src/components/hitItem.js
+++ src/components/hitItem.js
@@ -10,7 +10,13 @@ export class HitItem extends React.Component{
         return(
             <div className="col-md-4" key={this.props.hit.id}>
                 <div className="card">
-                    <div className="card-header card-title">{ !this.props.showDetails?this.props.hit.tags:'Details'} | {this.props.hit.webformatWidth} x {this.props.hit.webformatHeight}
+                    <div className='m-2'>
+                        {
+                            (this.props.showDetails)?<Link  className='btn btn-danger' to={'/hits'}> Back</Link> :''
+                        }
+
+                    </div>
+                    <div className="card-header card-title mb-2">{ !this.props.showDetails?this.props.hit.tags:'Details'} | {this.props.hit.webformatWidth} x {this.props.hit.webformatHeight}
                     </div>
                     <div className="card-body">
                         {
@@ -28,7 +34,25 @@ export class HitItem extends React.Component{
                     <div>
 
                     <Link  className= 'btn btn-link' to= {'/hitDetails/'+this.props.hit.id}> Hit details </Link>
-                    </div>:''
+                    </div>:
+                      <div className='row p-2'>
+                         
+                          <div className='col-auto'>
+
+                              <img  className='card-img'   src={this.props.hit.userImageURL} alt="description"/>
+                          </div>
+                           <div>
+                               <ul className='nav nav-pills'>
+                                   <li className='list-group-item'><span className='card-title'> Views:{this.props.hit.views}</span></li>
+                                   <li className='list-group-item'> <span className='card-title'> Comments:{this.props.hit.comments}</span></li>
+                                   <li className='list-group-item'> <span className='card-title'>Downlaod:{this.props.hit.downloads}</span></li>
+
+                               </ul>
+                               <Link  className='btn btn-danger' to={'/hits'}> Back</Link>
+                           </div>
+
+                      </div>
+
 
 
                 }
